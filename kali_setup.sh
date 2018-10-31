57b780eaa83ce9d486512b11893c52d61fcf08a6
Motosploit <Motosploit@gmail.com>
2018-10-31 14:43:09 -0400

    Added hardneing scripts
    Added some hardening scripts courtesy of https://gist.github.com/NitescuLucian/

--- kali_setup.sh
+++ kali_setup.sh
@@ -6,6 +6,7 @@
 # add firewall hardening using ufw
 # cronjob for hourly updates/upgrades
 # use ssh keys instead of username/password if needed
+# change hostname
 
 #set user variable
 user=$1
@@ -99,13 +100,41 @@ cd
 #sudo nano /etc/hostname 
 #sudo /etc/init.d/hostname.sh 
 
-#cronjob for hourly updates/upgrades, this isnt working yet
+#cronjob for hourly updates/upgrades, had some issues with this
 cd /home/$user
-
 #write out current crontab
 #echo new cron into cron file
 #install new cron file
 su -c "crontab -l > mycron; echo '0 * * * * sudo apt-get update && sudo apt-get upgrade -y' >>mycron; crontab mycron; rm mycron" -s /bin/sh $user
 
-
-
+#harden the image,
+echo Applying hardening scripts from https://gist.github.com/NitescuLucian/
+#chkrootkit examines certain elements of the target system and determines whether they have been tampered with.
+sudo apt-get install chkrootkit
+updatedb
+#check for any users that dont need a password to login
+cat /etc/shadow | awk -F: '($2==""){print $1}' > ./no_password_users.txt
+cat ./no_password_users.txt
+echo Running chkrootkit!
+sudo chkrootkit > ./chkrootkit_log.txt
+#Lynis is a security auditing tool for Linux, Mac OSX, and UNIX systems. It checks the system and the software configuration, to see if there is any room for improvement in the security defenses.
+sudo apt-get install lynis
+sudo apt-get -f install
+sudo apt-get install lynis
+#send lynis logs to file to current working directory
+echo Running lynis!
+sudo lynis audit system > ./lynis_log.txt
+##send open ports file to current working directory
+echo "Checking for open ports!"
+sudo netstat -tulpn > /tmp/open_ports_log.txt
+#information on how to close the open ports
+echo Close Unwanted Ports using: iptables -A INPUT -p tcp --dport PORT_NUMBER -j DROP 
+#output iptables settings to file in working directory
+sudo iptables -L -n -v > ./iptables_log.txt
+#display results of iptables check
+#cat ./iptables_log.txt
+#rkhunter  is  a  shell  script  which carries out various checks on the local system to try and detect known rootkits and malware.
+sudo apt-get install rkhunter
+sudo rkhunter --update
+sudo rkhunter -c
+echo "Please check no_password_users.txt, lynis_log.txt, and open_ports_log.txt to check for any additional actions you need to take."
